e7d1bcf7fe7ce8c25525020a83d4bdaf2977b569
pika298 <kit2008@live.com>
2016-10-19 01:14:32 +0800

    Completed category functions

--- app/models/category.server.model.js
+++ app/models/category.server.model.js
@@ -4,37 +4,38 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 /**
  * Validation
  */
-function validateLength (v)
-{
-		return v.length <= 15;
+function validateLength (v) {
+  // a custom validation function for checking string length
+  return v.length <= 15;
 }
+
 /**
  * Category Schema
  */
 var CategorySchema = new Schema({
-	// Category model fields   
-	create: {
-		type: Date,
-		default: Date.now
-	},
-	description: {
-		type: String,
-		default: '',
-		trim: true
-	},
-	name: {
-		type: String,
-		default: '',
-		trim: true,
-		unique: true,
-		required: 'name cannot be blank',
-		validate: [validateLength, 'name must be 15 chars in length or less']
-	}
+    created: {          // the property name
+        type: Date,     // types are defined e.g. String, Date, Number - http://mongoosejs.com/docs/guide.html
+        default: Date.now
+    },
+    description: {
+        type: String,
+        default: '',
+        trim: true      // types have specific functions e.g. trim, lowercase, uppercase - http://mongoosejs.com/docs/api.html#schema-string-js
+    },
+    name: {
+        type: String,
+        default: '',
+        trim: true,     
+        unique : true,
+        required: 'name cannot be blank',
+        validate: [validateLength, 'name must be 15 chars in length or less'] // wires into our custom validator function - http://mongoosejs.com/docs/api.html#schematype_SchemaType-validate
+    }
 });
 
+// Expose the model to other objects (similar to a 'public' setter).
 mongoose.model('Category', CategorySchema);
